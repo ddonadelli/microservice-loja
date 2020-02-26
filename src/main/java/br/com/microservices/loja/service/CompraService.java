7148b83d05419f799707c63af980073a74a65e80
ddonadelli <davi_donadelli@live.com>
2020-02-26 18:00:39 -0300

    Adicionando arquivos do vscode no gitignore; Anotação @Service na classe CompraService

--- src/main/java/br/com/microservices/loja/service/CompraService.java
+++ src/main/java/br/com/microservices/loja/service/CompraService.java
@@ -4,13 +4,15 @@ import br.com.microservices.loja.controller.dto.CompraDTO;
 import br.com.microservices.loja.controller.dto.InfoFornecedorDTO;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class CompraService {
     public void realizaCompra(CompraDTO compra) {
 
         RestTemplate cliente = new RestTemplate();
-        ResponseEntity<InfoFornecedorDTO> exchange = cliente.exchange("http://localhost:8081/info"+compra.getEndereco().getEstado(),
+        ResponseEntity<InfoFornecedorDTO> exchange = cliente.exchange("http://localhost:8081/info/"+compra.getEndereco().getEstado(),
                 HttpMethod.GET,
                 null,
                 InfoFornecedorDTO.class);
