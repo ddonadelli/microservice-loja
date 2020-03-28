5cde9c355c66415d404573d6e99b9673ff31b7a0
ddonadelli <davi_donadelli@live.com>
2020-03-27 21:17:29 -0300

    Adicionando o EUREKA

--- src/main/java/br/com/microservices/loja/LojaApplication.java
+++ src/main/java/br/com/microservices/loja/LojaApplication.java
@@ -2,9 +2,20 @@ package br.com.microservices.loja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class LojaApplication {
+
+	@LoadBalanced
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
--- src/main/java/br/com/microservices/loja/service/CompraService.java
+++ src/main/java/br/com/microservices/loja/service/CompraService.java
@@ -2,6 +2,8 @@ package br.com.microservices.loja.service;
 
 import br.com.microservices.loja.controller.dto.CompraDTO;
 import br.com.microservices.loja.controller.dto.InfoFornecedorDTO;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -9,13 +11,16 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CompraService {
-    public void realizaCompra(CompraDTO compra) {
 
-        RestTemplate cliente = new RestTemplate();
-        ResponseEntity<InfoFornecedorDTO> exchange = cliente.exchange("http://localhost:8081/info/"+compra.getEndereco().getEstado(),
+    @Autowired
+    private RestTemplate client;
+
+    public void realizaCompra(CompraDTO compra) {
+        ResponseEntity<InfoFornecedorDTO> exchange = client.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(),
                 HttpMethod.GET,
                 null,
                 InfoFornecedorDTO.class);
+        
 
         System.out.println(exchange.getBody().getEndereco());
     }
